ecf3c6c792c934e0287e9eb60ba87b3cb3bcecff
JeongWooYeong <wjd030811@gmail.com>
2021-01-11 20:46:41 +0900

    [FIX] 예외 접근 범위 public으로, 예외 처리 로직 추가

--- CRUD/exception/exception.go
+++ CRUD/exception/exception.go
@@ -4,28 +4,37 @@ import (
 	"fmt"
 	"github.com/valyala/fasthttp"
 	"log"
+	"strconv"
 )
 
 const(
 	invalidParameter = "X0400"
 	existingEmail = "X0409"
+	internalServerError = "E0500"
 )
 
-type exception struct {
+type Exception struct {
 	errorCode string
 	message   string
 	status    int
 }
 
-func (e exception) Error() string {
+func (e Exception) Error() string {
 	return fmt.Sprintf("[%s] %s", e.errorCode, e.message)
 }
 
-func Raise(e error) {
+func ToException(errorString string) (exception Exception, err error) {
+	exception.errorCode = errorString[1:6]
+	exception.message = errorString[8:]
+	exception.status, err = strconv.Atoi(exception.errorCode[2:])
+
+	return
+}
+
+func Raise(e Exception) {
 	log.Panic(e)
 }
 
-func RaiseException(ctx *fasthttp.RequestCtx, f func() *exception) {
-	e := f()
+func RaiseException(ctx *fasthttp.RequestCtx, e Exception) {
 	ctx.Error(e.Error(), e.status)
 }
