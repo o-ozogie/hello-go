b760631716593f7f20cfa321317543302fd8b9ef
JeongWooYeong <wjd030811@gmail.com>
2021-01-11 21:02:07 +0900

    [ADD] 인터페이스 구현에 따른 공통 메소드 구현

--- CRUD/request/user.go
+++ CRUD/request/user.go
@@ -1,11 +1,15 @@
 package request
 
+import (
+	"github.com/valyala/fasthttp"
+)
+
 type SignUpRequest struct {
 	Email string `json:"email"`
 	Password string `json:"password"`
 	Name string `json:"name"`
 }
 
-func (request SignUpRequest) Validate() bool {
-	return !(request.Email == "" || request.Password == "" || request.Name == "")
+func (request *SignUpRequest) Unmarshal(ctx *fasthttp.RequestCtx) {
+	unmarshal(ctx, request)
 }
