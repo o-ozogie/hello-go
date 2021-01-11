bf1081cdad1d173a06aef689c959bce3bb2b2de0
JeongWooYeong <wjd030811@gmail.com>
2021-01-11 21:01:30 +0900

    [FIX] validate 미사용에 따른 삭제 및 request unmarshalling

--- CRUD/request/request.go
+++ CRUD/request/request.go
@@ -1,5 +1,17 @@
 package request
 
+import (
+	"encoding/json"
+	"github.com/valyala/fasthttp"
+	"hello-go/CRUD/exception"
+)
+
 type Request interface {
-	Validate() bool
+	Unmarshal(ctx *fasthttp.RequestCtx)
+}
+
+func unmarshal(ctx *fasthttp.RequestCtx, request Request) {
+	if json.Unmarshal(ctx.Request.Body(), request) != nil {
+		exception.RaiseException(ctx, exception.InvalidParameterException())
+	}
 }
